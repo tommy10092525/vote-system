091cf7a8a5167e6a0568406cb0b34ee2683846ef
tommy10092525 <tommyxla777@gmail.com>
2024-11-18 01:24:52 +0900

    api getの引数を削除

--- app/kigyokeiei/api/get/route.ts
+++ app/kigyokeiei/api/get/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { createClient } from "@/utils/supabase/server";
 
-export async function GET(request: Request) {
+export async function GET() {
   const supabase=await createClient();
   const {data,count,error} = await supabase.from("votes").select(`
     num,
