50fbb2139fba9c8b380f1354038854754dc39454
tommy10092525 <tommyxla777@gmail.com>
2024-11-18 01:59:31 +0900

    マイナスの票数が表示される問題を強制修正

--- app/kigyokeiei/components/Result.tsx
+++ app/kigyokeiei/components/Result.tsx
@@ -3,13 +3,16 @@ import React from 'react'
 type Props={num:number,trueCount:number,falseCount:number}
 
 export default function ResultShow(props:Props) {
-  const {num,trueCount,falseCount}=props;
+  let {num,trueCount,falseCount}=props;
+  const d=Math.min(trueCount,falseCount,0);
+  trueCount+=d
+  falseCount+=d
   return (
     <div>
       <p>{`問${num}`}</p>
       <p>{`「正しい」:${trueCount}/「誤り」:${falseCount}`}</p>
       {
-        trueCount !== 0 && falseCount !== 0
+        trueCount +falseCount !== 0
           ? <p>{`${Math.round(100 * trueCount / (trueCount + falseCount))}%/${Math.round(100 * falseCount / (trueCount + falseCount))}%`}</p>
           : <></>}
     </div>
