00c0993ebd5cda981abd3574ba6f0aaa2f079ff5
tommy10092525 <tommyxla777@gmail.com>
2024-11-26 19:54:50 +0900

    localstorageに由来するundefinedのエラーを修正

--- app/kigyokeiei/page.tsx
+++ app/kigyokeiei/page.tsx
@@ -18,7 +18,6 @@ export default function () {
   for(let i=0;i<10;i++){
     results.push({num:i+1,myVote:{num:0,answer:false},isVoted:false})
   }
-  console.log(localStorage.getItem("votes"))
   return (
     <div className='float-left bg-gray-900 p-3 w-full text-white'>
       <div className='max-w-xl mx-auto'>
