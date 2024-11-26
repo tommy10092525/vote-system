b31793d92aaca3355db33f62b8c11e62238f8f20
tommy10092525 <tommyxla777@gmail.com>
2024-11-26 19:44:23 +0900

    整形

--- app/kigyokeiei/api/post/route.ts
+++ app/kigyokeiei/api/post/route.ts
@@ -9,7 +9,11 @@ export async function POST(request: Request) {
   
   const data:Post=await request.json();
   console.log(data)
-  const {error} = await supabase.from("votes").insert({num:data.num,answer:data.answer,change:data.change})
+  const {error} = await supabase.from("votes").insert({
+    num:data.num,
+    answer:data.answer,
+    change:data.change
+  })
   console.log(error);
-  return NextResponse.json([{num:1,answer:true},{num:2,answer:false}])
+  return NextResponse.json({success:true})
 }
