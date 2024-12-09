437063b63090cd7ce34028b7a76a0b0322a73b94
tommy10092525 <tommyxla777@gmail.com>
2024-12-10 00:57:25 +0900

    送信ボタンに非同期関数を設定しstateの問題を修正

--- app/kigyokeiei/components/VoteButton.tsx
+++ app/kigyokeiei/components/VoteButton.tsx
@@ -6,32 +6,30 @@ type Props = {
   num: number,
   myAnswers:Vote[],
   publicAnswers:PublicVote[],
-  handleMyAnswerChange:(num:number,answer:"correct"|"incorrect",change:number)=>void
+  handleMyAnswerChange:(num:number,answer:"correct"|"incorrect",change:number)=>Promise<void>
 }
 
 
 const VoteButton = (props: Props) => {
-  // console.log(props);
   const myAnswer = props.myAnswers.find((item)=>item.num===props.num)?.answer;
-  // console.log(myAnswer);
   const [isPosting, setIsPosting] = useState(false);
 
   return (
     <div className={`w-full my-2 pl-2 ${myAnswer==="none" ? "" : "border-blue-500 border-l-4"}`}>
-      <p>{`問${String(props.num)}${isPosting ? " 送信中..." : ""}`}</p>
+      <p className='text-2xl'>{`問${String(props.num)}${isPosting ? " 送信中..." : ""}`}</p>
       <div className='w-full p-1'>
         <button
           className={`w-full shadow p-1 rounded-md text-black ${myAnswer==="correct" ? "bg-blue-700" : "bg-blue-200"}`}
-          onClick={() => {
+          onClick={async () => {
             console.log("correct")
             if (!isPosting && (myAnswer === "none" || myAnswer === "incorrect")) {
               setIsPosting(true);
               if (myAnswer === "incorrect") {
                 // 前回の投票を相殺
-                props.handleMyAnswerChange(props.num, "incorrect", -1);
+                await props.handleMyAnswerChange(props.num, "incorrect", -1);
               }
               // 新しい投票を送信
-              props.handleMyAnswerChange(props.num, "correct", 1);
+              await props.handleMyAnswerChange(props.num, "correct", 1);
               setIsPosting(false);
             }
           }}
@@ -42,16 +40,16 @@ const VoteButton = (props: Props) => {
       <div className='w-full p-1'>
         <button
           className={`w-full shadow p-1 rounded-md text-black ${myAnswer==="incorrect" ? "bg-pink-700" : "bg-pink-200"}`}
-          onClick={() => {
+          onClick={async () => {
             console.log("incorrect")
             if (!isPosting && (myAnswer === "none" || myAnswer === "correct")) {
               setIsPosting(true);
               if (myAnswer === "correct") {
                 // 前回の投票を相殺
-                props.handleMyAnswerChange(props.num, "correct", -1);
+                await props.handleMyAnswerChange(props.num, "correct", -1);
               }
               // 新しい投票を送信
-              props.handleMyAnswerChange(props.num, "incorrect", 1);
+              await props.handleMyAnswerChange(props.num, "incorrect", 1);
               setIsPosting(false);
             }
           }}
