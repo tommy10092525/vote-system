6e9aeb589a71828f8ca1770221158124be350b90
tommy10092525 <tommyxla777@gmail.com>
2024-12-23 15:45:59 +0900

    開始時刻と終了時刻を変更

--- app/kigyokeiei/constants/constants.ts
+++ app/kigyokeiei/constants/constants.ts
@@ -1,4 +1,4 @@
-const start=new Date("2025-01-27T11:00:00Z")
-const end=new Date("2025-01-27T11:30:00Z")
+const start=new Date("2025-01-27T11:10:00Z")
+const end=new Date("2025-01-27T11:40:00Z")
 
 export {start,end}
