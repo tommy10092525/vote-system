27b31c2d3acdc1ed1b6bd4f1da581a5612b03a69
tommy10092525 <tommyxla777@gmail.com>
2025-01-20 15:10:45 +0900

    テスト時刻変更

--- app/kigyokeiei/constants/constants.ts
+++ app/kigyokeiei/constants/constants.ts
@@ -1,5 +1,5 @@
 const start=new Date("2025-01-27T11:00:00Z")
-const end=new Date("2025-01-27T11:30:00Z")
+const end=new Date("2025-01-27T12:00:00Z")
 const email="ryate872@nekochan.fr"
 
 export {start,end,email}
