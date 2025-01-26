c26e36887cd61b7fabed7729af3ae9e038a2e88a
tommy10092525 <tommyxla777@gmail.com>
2025-01-27 08:25:27 +0900

    Update start and end times in constants.ts

--- app/kigyokeiei/constants/constants.ts
+++ app/kigyokeiei/constants/constants.ts
@@ -1,5 +1,5 @@
-const start=new Date("2025-01-27T11:10:00Z")
-const end=new Date("2025-01-27T12:50:00Z")
+const start=new Date("2025-01-27T2:10:00Z")
+const end=new Date("2025-01-27T3:50:00Z")
 const email="ryate872@nekochan.fr"
 
 export {start,end,email}
